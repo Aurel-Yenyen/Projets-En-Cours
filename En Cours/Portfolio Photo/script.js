7cffcd7fc232afb04f1449fe4cc813067c4c1940
Aurélien <aurel.yenyen70@gmail.com>
2022-07-29 16:16:11 +0200

    Ajout Partie de Faredj

--- En Cours/Portfolio Photo/script.js
+++ En Cours/Portfolio Photo/script.js
@@ -1,15 +1,1 @@
-$( document ).ready(function() {
-
-    $( ".burger" ).click(function() {
-    $( ".navbar" ).slideToggle( "slow", function() {
-    $( ".burger" ).hide();
-    });
-    });
-    
-    $( ".burger" ).click(function() {
-    $( ".navbar" ).slideToggle( "slow", function() {
-    $( ".burger" ).show();
-    });
-    });
-    
-    });
+    inifi
