80ad2f7f2cfa96a3d35743aba8e35e50358e3369
Aurel-Yenyen <aurel-yenyen@outlook.fr>
2022-08-04 01:04:38 +0200

    New

---
+++ Exercice Javascript/Agrandir un Element/script.js
@@ -0,0 +1,31 @@
+let titre = document.getElementById("titre");
+
+titre.addEventListener("click", changetext)
+titre.addEventListener("mousedown", reset)
+
+
+function changetext(){
+    this.style.color = 'orange';
+    this.style.fontSize = '40px';
+}
+
+function reset(){
+    this.style.color = "";
+    this.style.fontSize = "";
+}
+
+let number = 20;
+let compte = document.getElementById("compte");
+
+const body = document.querySelector('body')
+
+for (let number = 20; number >= 0; number-=2){
+    const para = document.createElement('p')
+    para.innerHTML = number
+    body.append (para)
+    compte.innerHTML += number + " ";
+}
+
+
+para.innerHTML = "Bonjour"
+
