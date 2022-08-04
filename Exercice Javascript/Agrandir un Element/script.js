41c06f15d036a45b56d0146d19c8db12a622d5de
Aurel-Yenyen <aurel-yenyen@outlook.fr>
2022-08-04 22:03:03 +0200

    New

--- Exercice Javascript/Agrandir un Element/script.js
+++ Exercice Javascript/Agrandir un Element/script.js
@@ -1,30 +1,47 @@
 let titre = document.getElementById("titre");
 
-const taille = ["18px", "40px", "60px"];
 titre.addEventListener("click", changetext)
+
+let compte = 0  //Avec Troisième Façon
+
+let change = true //Avec Deuxième Façon
 
 
 function changetext(){
-    const number = Math.floor(Math.random()*taille.length);
-    if (number == 0){
-        this.style.fontSize = taille[0]
-        console.log(number)
+    //Première Façon, la moins couteuse car aucune variable, juste un test
+    if (this.style.fontSize != "40px"){
+        this.style.fontSize = "40px"
     }else{
-        this.style.fontSize = taille[1]
-        console.log(number)
+        this.style.fontSize = "25px"
     }
+    //Deuxième Façon
+    if (change == false){
+        this.style.fontSize = "40px"
+        change = true
+    }else{
+        this.style.fontSize = "25px"
+        change = false
+    }
+    //Troisième Façon, plus couteuse car risque de rupture de la mémoire "Stock Overflow" à cause de l'incréentation qui ne se réinitialise pas.
+    if ((compte % 2) == 0){
+        this.style.fontSize = "40px"
+    }else{
+        this.style.fontSize = "25px"
+    }
+    compte++
+    
  }
 
-let number = 20;
-let compte = document.getElementById("compte");
+// let number = 20;
+// let compte = document.getElementById("compte");
 
-const body = document.querySelector('body')
+// const body = document.querySelector('body')
 
-for (let number = 20; number >= 0; number-=2){
-    let para = document.createElement('p')
-    para.innerHTML = number
-    body.append (para)
-    compte.innerHTML += number + " ";
-}
+// for (let number = 20; number >= 0; number-=2){
+//     let para = document.createElement('p')
+//     para.innerHTML = number
+//     body.append (para)
+//     compte.innerHTML += number + " ";
+// }
 
 
