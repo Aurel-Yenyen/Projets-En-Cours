83a44f1af1ce33a20fa8052952ad88d1219b36a6
Aurel-Yenyen <aurel-yenyen70@live.fr>
2022-08-04 11:18:11 +0100

    Chnage

--- Exercice Javascript/Agrandir un Element/script.js
+++ Exercice Javascript/Agrandir un Element/script.js
@@ -1,31 +1,37 @@
 let titre = document.getElementById("titre");
 
+
 titre.addEventListener("click", changetext,)
-titre.removeEventListener("click" , reset,)
 
 
 function changetext(){
-    this.style.color = 'orange';
-    this.style.fontSize = '40px';
+    let clique = 0;
+    while (clique == 0){
+            this.style.color = 'blue';
+            this.style.fontSize = ''; 
+        if (clique += 1){
+            this.style.color = 'red';
+            this.style.fontSize = '';
+    }
+    }
+
+
+    
+    console.log(clique)
 }
 
-function reset(){
-    this.style.color = "";
-    this.style.fontSize = "";
-}
+// let number = 20;
+// let compte = document.getElementById("compte");
 
-let number = 20;
-let compte = document.getElementById("compte");
+// const body = document.querySelector('body')
 
-const body = document.querySelector('body')
-
-for (let number = 20; number >= 0; number-=2){
-    const para = document.createElement('p')
-    para.innerHTML = number
-    body.append (para)
-    compte.innerHTML += number + " ";
-}
+// for (let number = 20; number >= 0; number-=2){
+//     let para = document.createElement('p')
+//     para.innerHTML = number
+//     body.append (para)
+//     compte.innerHTML += number + " ";
+// }
 
 
-para.innerHTML = "Bonjour"
+// para.innerHTML = "Bonjour"
 
