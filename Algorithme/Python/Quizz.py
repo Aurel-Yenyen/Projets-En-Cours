d6e4f36de8b801dfa71d405f96094efcf7d5363f
Aurel-Yenyen <aurel-yenyen70@live.fr>
2022-08-14 11:45:33 +0100

    Quizz

---
+++ Algorithme/Python/Quizz.py
@@ -0,0 +1,8 @@
+print("Bienvenue sur le Quizz de Yenyen")
+print("")
+print("De quelle couleur est le cheval blanc d'Henri IV?")
+print("")
+print("""Réponse 1: Bleu
+Réponse 2: Rouge
+Réponse 3: Blanc
+Réponse 4: Vert""")
