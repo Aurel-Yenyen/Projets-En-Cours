0b92679e5b6b1ea05965c0b55c10d8a8868f43b2
Aurel-Yenyen <aurel-yenyen70@live.fr>
2022-09-06 08:50:38 +0100

    Change Code

--- Exercice Javascript/Caroussel/app.js
+++ Exercice Javascript/Caroussel/app.js
@@ -1,57 +1,69 @@
 let input = document.getElementById('input')
 let img = document.getElementById('image')
+let cross1 = document.getElementById('radio1')
+let cross2 = document.getElementById('radio2')
+let cross3 = document.getElementById('radio3')
+let cross4 = document.getElementById('radio4')
+let cross5 = document.getElementById('radio5')
+let cross6 = document.getElementById('radio6')
 
 function reprise(){
     console.log("c'est bon")
     img.style.animation = "spin 20s ease-in-out infinite";
 }
 
-function choix() {
+input.addEventListener('click', choix => {
 
-    let cross1 = document.getElementById('radio1')
-    let cross2 = document.getElementById('radio2')
-    let cross3 = document.getElementById('radio3')
-    let cross4 = document.getElementById('radio4')
-    let cross5 = document.getElementById('radio5')
-    let cross6 = document.getElementById('radio6')
+
 
     if (cross1.checked){
         img.style.animation = 'none'
         img.style.transform = 'translateX(0px)'
-        console.log(cross1);
+        // console.log(cross1);
         setTimeout(reprise, 5000)
         }
     else if (cross2.checked){
         img.style.animation = 'none'
         img.style.transform = 'translateX(-200px)'
-        console.log(cross2);  
+        // console.log(cross2);  
         setTimeout(reprise, 5000)   
     }
     else if (cross3.checked){
         img.style.animation = 'none'
         img.style.transform = 'translateX(-405px)'
-        console.log(cross3);     
+        // console.log(cross3);     
         setTimeout(reprise, 5000)
     }
     else if (cross4.checked){
         img.style.animation = 'none'
         img.style.transform = 'translateX(-610px)'
-        console.log(cross4);   
+        // console.log(cross4);   
         setTimeout(reprise, 5000)  
     }
     else if (cross5.checked){
         img.style.animation = 'none'
         img.style.transform = 'translateX(-815px)'
-        console.log(cross5);    
+        // console.log(cross5);    
         setTimeout(reprise, 5000) 
     }
     else if (cross6.checked){
         img.style.animation = 'none'
         img.style.transform = 'translateX(-1020px)'
-        console.log(cross6); 
+        // console.log(cross6); 
         setTimeout(reprise, 5000)    
     }
 
+})
+
+function check(){
+    if (img.style.transform = 'translateX(0px)'){
+        cross1.checked = true
+        console.log(radio1);
+    }
+    else if (img.style.transform = 'translateX(-200px)'){
+        cross1.checked = true
+    }
 }
 
+check()
                             
