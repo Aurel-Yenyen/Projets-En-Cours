be659efa54f437b915d0562bdcae6ab54521e449
Aurel-Yenyen <aurel-yenyen70@live.fr>
2022-09-06 15:22:57 +0100

    Ajout  Space Invaders

---
+++ Exercice Javascript/SpaceInvaders/app.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById('fond')
+
+// Style du Canvas
+
+canvas.style.background = '#383838'
+canvas.style.width = "800px"
+canvas.style.height = "500px"
+
+//Vaisseau
+
+let command = document.getElementById('shipe1')
+let enemi = document.getElementById('shipe2')
