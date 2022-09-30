1a0d497456b04857cde0ab618e9edc9eec9cf3d7
Aurel-Yenyen <aurel-yenyen70@live.fr>
2022-09-30 15:24:59 +0100

    Change Projet Final

--- En Cours/Projet Final 2.0/app.js
+++ En Cours/Projet Final 2.0/app.js
@@ -1,3 +1,6 @@
+
+// ----------------   CAROUSSEL   -----------------
+
 let input = document.getElementById('input')
 let img = document.getElementById('images')
 let cross1 = document.getElementById('radio1')
@@ -70,36 +73,263 @@ input.addEventListener('click', choix => {
 function check(){
     if (image1.style.transform = 'translateX(0px)'){
         cross1.checked = true
-        console.log(image1);
+
     }
     else if (image2.style.transform = 'translateX(0px)'){
         cross2.checked = true
-        console.log(image2);
+
     }
     else if (image3.style.transform = 'translateX(0px)'){
         cross3.checked = true
-        console.log(image3);
+
     }
     else if (image4.style.transform = 'translateX(0px)'){
         cross4.checked = true
-        console.log(image4);
+
     }
     else if (image5.style.transform = 'translateX(0px)'){
         cross5.checked = true
-        console.log(image5);
+
     }
     else if (image6.style.transform = 'translateX(0px)'){
         cross6.checked = true
-        console.log(image6);
+
     }
     else if (image7.style.transform = 'translateX(0px)'){
         cross7.checked = true
-        console.log(image7);
+
     }
     else if (image8.style.transform = 'translateX(0px)'){
         cross8.checked = true
-        console.log(image8);
+
     }
 }
 
 check()
+
+// ----------------   CCOMPTEUR 1   -----------------
+
+function countTo1(){
+    let from = 0;
+    let to = 232;
+    let step = to > from ? 2 : -1
+    let interval = 1
+
+    if (from == to){
+        document.querySelector('#compteur1').textContent = from
+        return
+    }
+    let counter = setInterval(function(){
+        from += step
+        document.querySelector('#compteur1').textContent = from
+
+        if (from == to){
+            clearInterval(counter)
+        }
+    }, interval)
+}
+countTo1()
+
+// ----------------   CCOMPTEUR 2   -----------------
+
+function countTo2(){
+    let from = 0;
+    let to = 521;
+    let step = to > from ? 1 : -1
+    let interval = 1
+
+    if (from == to){
+        document.querySelector('#compteur2').textContent = from
+        return
+    }
+    let counter = setInterval(function(){
+        from += step
+        document.querySelector('#compteur2').textContent = from
+
+        if (from == to){
+            clearInterval(counter)
+        }
+    }, interval)
+}
+countTo2()
+
+// ----------------   CCOMPTEUR 3   -----------------
+
+function countTo3(){
+    let from = 0;
+    let to = 1463;
+    let step = to > from ? 7 : -1
+    let interval = 1
+
+    if (from == to){
+        document.querySelector('#compteur3').textContent = from
+        return
+    }
+    let counter = setInterval(function(){
+        from += step
+        document.querySelector('#compteur3').textContent = from
+
+        if (from == to){
+            clearInterval(counter)
+        }
+    }, interval)
+}
+countTo3()
+
+// ----------------   CCOMPTEUR 3   -----------------
+
+function countTo4(){
+    let from = 0;
+    let to = 15;
+    let step = to > from ? 1 : -1
+    let interval = 1
+
+    if (from == to){
+        document.querySelector('#compteur4').textContent = from
+        return
+    }
+    let counter = setInterval(function(){
+        from += step
+        document.querySelector('#compteur4').textContent = from
+
+        if (from == to){
+            clearInterval(counter)
+        }
+    }, interval)
+}
+countTo4()
+
+// ----------------   TABS   -----------------
+
+let bouton = document.getElementById('nav-link-bar'),
+    btn1 = document.getElementById('nav-link1'),
+    btn2 = document.getElementById('nav-link2'),
+    btn3 = document.getElementById('nav-link3'),
+    btn4 = document.getElementById('nav-link4'),
+    contentText1 = document.getElementById('content-text1'),
+    contentText2 = document.getElementById('content-text2'),
+    contentText3 = document.getElementById('content-text3'),
+    contentText4 = document.getElementById('content-text4'), 
+    imgText1 = document.getElementById('img-btn1'),
+    imgText2 = document.getElementById('img-btn2'),
+    imgText3 = document.getElementById('img-btn3'),
+    imgText4 = document.getElementById('img-btn4'),
+    change = true,
+    change2 = true,
+    change3 = true,
+    change4 = true
+
+btn1.addEventListener('click', changeBtn1 =>{
+
+    if (change == false){
+
+        btn1.style.background = '#fff'
+        btn1.style.border = '1px solid black'
+        btn1.style.color = '#000'
+        change = true
+    }
+
+
+    btn1.style.background = '#D73232'
+    btn1.style.border = 'none'
+    btn1.style.color = '#fff'
+    btn2.style.background = '#fff'
+    btn2.style.border = '1px solid black'
+    btn2.style.color = '#000'
+    btn3.style.background = '#fff'
+    btn3.style.border = '1px solid black'
+    btn3.style.color = '#000'
+    btn4.style.background = '#fff'
+    btn4.style.border = '1px solid black'
+    btn4.style.color = '#000'
+    contentText1.style.visibility = 'visible'
+    contentText2.style.visibility = 'hidden'
+    contentText3.style.visibility = 'hidden'
+    contentText4.style.visibility = 'hidden'
+    change = false
+})
+
+
+
+btn2.addEventListener('click', changeBtn2 =>{
+
+    if (change2 == false){
+        btn2.style.background = '#fff'
+        btn2.style.border = '1px solid black'
+        btn2.style.color = '#000'
+        change2 = true
+    }
+
+        btn2.style.background = '#D73232'
+        btn2.style.border = 'none'
+        btn2.style.color = '#fff'
+        btn1.style.background = '#fff'
+        btn1.style.border = '1px solid black'
+        btn1.style.color = '#000'
+        btn3.style.background = '#fff'
+        btn3.style.border = '1px solid black'
+        btn3.style.color = '#000'
+        btn4.style.background = '#fff'
+        btn4.style.border = '1px solid black'
+        btn4.style.color = '#000'
+        contentText1.style.visibility = 'hidden'
+        contentText2.style.visibility = 'visible'
+        contentText3.style.visibility = 'hidden'
+        contentText4.style.visibility = 'hidden'
+        change2 = false
+})
+btn3.addEventListener('click', changeBtn3 =>{
+
+    if (change3 == false){
+        btn3.style.background = '#fff'
+        btn3.style.border = '1px solid black'
+        btn3.style.color = '#000'
+        change3 = true
+    }
+
+        btn3.style.background = '#D73232'
+        btn3.style.border = 'none'
+        btn3.style.color = '#fff'
+        btn1.style.background = '#fff'
+        btn1.style.border = '1px solid black'
+        btn1.style.color = '#000'
+        btn2.style.background = '#fff'
+        btn2.style.border = '1px solid black'
+        btn2.style.color = '#000'
+        btn4.style.background = '#fff'
+        btn4.style.border = '1px solid black'
+        btn4.style.color = '#000'
+        contentText1.style.visibility = 'hidden'
+        contentText2.style.visibility = 'hidden'
+        contentText3.style.visibility = 'visible'
+        contentText4.style.visibility = 'hidden'
+        change3 = false
+})
+btn4.addEventListener('click', changeBtn4 =>{
+
+    if (change4 == false){
+        btn4.style.background = '#fff'
+        btn4.style.border = '1px solid black'
+        btn4.style.color = '#000'
+        change4 = true
+    }
+
+        btn4.style.background = '#D73232'
+        btn4.style.border = 'none'
+        btn4.style.color = '#fff'
+        btn1.style.background = '#fff'
+        btn1.style.border = '1px solid black'
+        btn1.style.color = '#000'
+        btn2.style.background = '#fff'
+        btn2.style.border = '1px solid black'
+        btn2.style.color = '#000'
+        btn3.style.background = '#fff'
+        btn3.style.border = '1px solid black'
+        btn3.style.color = '#000'
+        contentText1.style.visibility = 'hidden'
+        contentText2.style.visibility = 'hidden'
+        contentText3.style.visibility = 'hidden'
+        contentText4.style.visibility = 'visible'
+        change4 = false
+})
+
