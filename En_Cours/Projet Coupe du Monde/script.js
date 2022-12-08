4357316b657ff8ba6c7a4b115d11fc610694fbdc
Aurélien <aurel.yenyen70@gmail.com>
2022-12-08 08:41:18 +0100

    Ajout Carroussel CDM

--- En_Cours/Projet Coupe du Monde/script.js
+++ En_Cours/Projet Coupe du Monde/script.js
@@ -26,110 +26,110 @@ let overlay6 = document.getElementById('overlay6')
 let overlay7 = document.getElementById('overlay7')
 let overlay8 = document.getElementById('overlay8')
 
-let cadre1 = document.getElementById('cadre-img1')
-let cadre2 = document.getElementById('cadre-img2')
-let cadre3 = document.getElementById('cadre-img3')
-let cadre4 = document.getElementById('cadre-img4')
-let cadre5 = document.getElementById('cadre-img5')
-let cadre6 = document.getElementById('cadre-img6')
-let cadre7 = document.getElementById('cadre-img7')
-let cadre8 = document.getElementById('cadre-img8')
+let cadre1 = document.getElementById('card1')
+let cadre2 = document.getElementById('card2')
+let cadre3 = document.getElementById('card3')
+let cadre4 = document.getElementById('card4')
+let cadre5 = document.getElementById('card5')
+let cadre6 = document.getElementById('card6')
+let cadre7 = document.getElementById('card7')
+let cadre8 = document.getElementById('card8')
 
-let image1 = document.getElementById('image1')
-let image2 = document.getElementById('image2')
-let image3 = document.getElementById('image3')
-let image4 = document.getElementById('image4')
-let image5 = document.getElementById('image5')
-let image6 = document.getElementById('image6')
-let image7 = document.getElementById('image7')
-let image8 = document.getElementById('image8')
+let cadre = document.getElementById('cards')
+let btnleft = document.getElementById('btnleft')
+let btnright = document.getElementById('btnright')
 
 let change = true
 
 cadre1.addEventListener('mouseover', window =>{
     overlay1.style.visibility = 'visible'
-    image1.style.transform = 'scale(1.2)'
-    image1.style.transition = ('transform 1s ease-in')
+    overlay1.style.transform = 'translateX(0vw)'
+    overlay1.style.transition = 'transform 1s'
 })
 cadre1.addEventListener('mouseleave', disapear =>{
     overlay1.style.visibility = 'hidden' 
-    image1.style.transform = 'scale(1)'
-    image1.style.transition = ('transform 1s ease-in')  
+    overlay1.style.transform = 'translateX(-6vw)'
+    overlay1.style.transition = 'transform 1s'
 })
 
 cadre2.addEventListener('mouseover', window =>{
     overlay2.style.visibility = 'visible'
-    image2.style.transform = 'scale(1.2)'
-    image2.style.transition = ('transform 1s ease-in')
+    overlay2.style.transform = 'translateX(0vw)'
+    overlay2.style.transition = 'transform 1s'
 })
 cadre2.addEventListener('mouseleave', disapear =>{
-    overlay2.style.visibility = 'hidden' 
-    image2.style.transform = 'scale(1)'
-    image2.style.transition = ('transform 1s ease-in')  
+    overlay2.style.visibility = 'hidden'  
+    overlay2.style.transform = 'translateX(-6vw)'
+    overlay2.style.transition = 'transform 1s'
 })
 
 cadre3.addEventListener('mouseover', window =>{
     overlay3.style.visibility = 'visible'
-    image3.style.transform = 'scale(1.2)'
-    image3.style.transition = ('transform 1s ease-in')
+    overlay3.style.transform = 'translateX(0vw)'
+    overlay3.style.transition = 'transform 1s'
 })
 cadre3.addEventListener('mouseleave', disapear =>{
-    overlay3.style.visibility = 'hidden' 
-    image3.style.transform = 'scale(1)'
-    image3.style.transition = ('transform 1s ease-in')  
+    overlay3.style.visibility = 'hidden'   
+    overlay3.style.transform = 'translateX(-6vw)'
+    overlay3.style.transition = 'transform 1s'
 })
 
 cadre4.addEventListener('mouseover', window =>{
     overlay4.style.visibility = 'visible'
-    image4.style.transform = 'scale(1.2)'
-    image4.style.transition = ('transform 1s ease-in')
+    overlay4.style.transform = 'translateX(0vw)'
+    overlay4.style.transition = 'transform 1s'
 })
 cadre4.addEventListener('mouseleave', disapear =>{
-    overlay4.style.visibility = 'hidden' 
-    image4.style.transform = 'scale(1)'
-    image4.style.transition = ('transform 1s ease-in')  
+    overlay4.style.visibility = 'hidden'  
+    overlay4.style.transform = 'translateX(-6vw)'
+    overlay4.style.transition = 'transform 1s' 
 })
 
 cadre5.addEventListener('mouseover', window =>{
     overlay5.style.visibility = 'visible'
-    image5.style.transform = 'scale(1.2)'
-    image5.style.transition = ('transform 1s ease-in')
+    overlay5.style.transform = 'translateX(0vw)'
+    overlay5.style.transition = 'transform 1s'
 })
 cadre5.addEventListener('mouseleave', disapear =>{
-    overlay5.style.visibility = 'hidden' 
-    image5.style.transform = 'scale(1)'
-    image5.style.transition = ('transform 1s ease-in')  
+    overlay5.style.visibility = 'hidden'  
+    overlay5.style.transform = 'translateX(-6vw)'
+    overlay5.style.transition = 'transform 1s' 
 })
 
 cadre6.addEventListener('mouseover', window =>{
     overlay6.style.visibility = 'visible'
-    image6.style.transform = 'scale(1.2)'
-    image6.style.transition = ('transform 1s ease-in')
+    overlay6.style.transform = 'translateX(0vw)'
+    overlay6.style.transition = 'transform 1s'
 })
 cadre6.addEventListener('mouseleave', disapear =>{
-    overlay6.style.visibility = 'hidden' 
-    image6.style.transform = 'scale(1)'
-    image6.style.transition = ('transform 1s ease-in')  
+    overlay6.style.visibility = 'hidden'   
+    overlay6.style.transform = 'translateX(-6vw)'
+    overlay6.style.transition = 'transform 1s'
 })
 
 cadre7.addEventListener('mouseover', window =>{
     overlay7.style.visibility = 'visible'
-    image7.style.transform = 'scale(1.2)'
-    image7.style.transition = ('transform 1s ease-in')
+    overlay7.style.transform = 'translateX(0vw)'
+    overlay7.style.transition = 'transform 1s'
 })
 cadre7.addEventListener('mouseleave', disapear =>{
-    overlay7.style.visibility = 'hidden' 
-    image7.style.transform = 'scale(1)'
-    image7.style.transition = ('transform 1s ease-in')  
+    overlay7.style.visibility = 'hidden'   
+    overlay7.style.transform = 'translateX(-6vw)'
+    overlay7.style.transition = 'transform 1s'
 })
 
 cadre8.addEventListener('mouseover', window =>{
     overlay8.style.visibility = 'visible'
-    image8.style.transform = 'scale(1.2)'
-    image8.style.transition = ('transform 1s ease-in')
+    overlay8.style.transform = 'translateX(0vw)'
+    overlay8.style.transition = 'transform 1s'
 })
 cadre8.addEventListener('mouseleave', disapear =>{
     overlay8.style.visibility = 'hidden' 
-    image8.style.transform = 'scale(1)'
-    image8.style.transition = ('transform 1s ease-in')   
+    overlay8.style.transform = 'translateX(-6vw)'
+    overlay8.style.transition = 'transform 1s' 
 })
+
+let count = 0
+
+
+
